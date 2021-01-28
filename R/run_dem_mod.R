51329fbf3c830b4885b31c783efd7b8ae7c1344d
Malavika Rajeev <mrajeev08@gmail.com>
2021-01-27 17:11:02 -0800

    adding vacc and dem sims

---
+++ R/run_dem_mod.R
@@ -0,0 +1,109 @@
+# Get demographic estimates for a set of bootstrapped data
+library(foreach)
+library(doParallel)
+library(iterators)
+library(dplyr)
+library(readr)
+library(here)
+library(tidyr)
+
+source("R/dem_models.R")
+
+# animals
+animals_2018 <- read_csv("data/animal_2018.csv")
+animals_2019 <- read_csv("data/campaign_2019.csv")
+
+# filter to dogs
+animals_2018 %>%
+  filter(Species == "A") %>%
+  mutate(year = 2018, 
+         AgeMonths = replace_na(AgeMonths, 0), 
+         AgeYears = replace_na(AgeYears, 0), 
+         age_mos = AgeYears*12 + AgeMonths) %>%
+  mutate(age_mos = ifelse(age_mos == 0, NA, age_mos), 
+         Sex = toupper(Sex)) %>%
+  select(Fokontany, Commune, Date, age_mos, year, Sex) -> animals_age
+
+animals_2019 %>%
+  filter(`Dog or Cat` == "dog") %>%
+  select(Date, age_mos = `Age (months)`, Commune, Fokontany = `Owner Location`, 
+         Sex) %>%
+  mutate(year = 2019, Sex = toupper(Sex)) %>%
+  bind_rows(animals_age) -> animals_age
+
+write_csv(animals_age, "out/animals_age.csv")
+
+# Bootstrapped data
+set.seed(1334)
+n_by_age <- (replicate(100, 
+                       ages_to_class(age_yrs = animals_age$age_mos/12, 
+                                     years_in_class = c(1, 1, 4, 1),
+                                     sample = TRUE, N = 1000)))
+n_by_age <- split(n_by_age, rep(1:ncol(n_by_age), each = nrow(n_by_age)))
+
+# original data
+original <- ages_to_class(age_yrs = animals_age$age_mos/12, 
+                          years_in_class = c(1, 1, 4, 1),
+                          sample = FALSE)
+n_by_age$`0` <- original
+  
+# Get expanded grid
+set.seed(1335)
+par_df <- data.frame(p1 = rnorm(100, mean = 0, sd = 1), 
+                     fert = rnorm(100, mean = 0, sd = 1))
+ll_grid <- expand_grid(par_df,  
+                       age_data = 1:length(n_by_age))
+
+# parallelize this (otherwise it takes a while...)
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+
+dem_ests <- 
+  foreach(i = iter(ll_grid, "row"), .combine = 'rbind', 
+          .export = c("les_mat", "exp_trans", "apply_trans")) %dopar% {
+    
+    # draw initial values
+    age_ests <- optim(par = list(p1 = i$p1, 
+                                 fert = i$fert), 
+                      fn = les_mat, n_per_age = n_by_age[[i$age_data]], 
+                      n_classes = 4, years_in_class = c(1, 1, 4, 1),
+                      par_trans = list(exp_trans, exp),
+                      trans_probs = rep("p1", 4), 
+                      type = "pois")
+    
+    preds <- les_mat(n_per_age = n_by_age[[i$age_data]], 
+                     par = age_ests$par, 
+                     n_classes = 4, years_in_class = c(1, 1, 4, 1),
+                     par_trans = list(exp_trans, exp),
+                     trans_probs = rep("p1", 4),
+                     type = "pred")
+    
+    par_ests <- apply_trans(age_ests$par, list(exp_trans, exp))
+    
+    # output
+    data.frame(ll = age_ests$value, 
+               sim = names(n_by_age[i$age_data]), 
+               psurv_adult_init = i$p1,
+               fert_init = i$fert,
+               fert_est = par_ests["fert"], 
+               psurv_adult_est = par_ests["p1"],
+               stable_ages_est = preds$stable_age, 
+               growth = preds$growth,
+               stable_ages_data = n_by_age[[i$age_data]], 
+               age_class = 1:length(preds$stable_age))
+               
+  }
+
+stopCluster(cl)
+
+# Parameter estimates
+dem_ests %>%
+  group_by(sim, growth) %>%
+  mutate(pop_growth = factor(ifelse(growth >= 1, "Growing", "Declining")), 
+         prop_data = stable_ages_data/sum(stable_ages_data), 
+         prop_est = stable_ages_est/sum(stable_ages_est)) -> dem_ests
+
+# Output for demography
+write_csv(dem_ests, "out/dem_ests.csv")
+
+
---
+++ R/run_vacc_mod.R
@@ -0,0 +1,113 @@
+# Get demographic estimates for a set of bootstrapped data
+library(foreach)
+library(doParallel)
+library(iterators)
+library(dplyr)
+library(readr)
+library(here)
+library(tidyr)
+source("R/dem_models.R")
+source("R/vacc_models.R")
+
+dem_ests <- read_csv("out/dem_ests.csv")
+
+# Sim vaccination with parameters where pop is growing -------------------------
+dem_ests %>%
+  ungroup() %>%
+  filter(pop_growth == "Growing") %>%
+  select(fert_est, psurv_adult_est, growth, prop_est, age_class, sim) %>%
+  pivot_wider(values_from = prop_est, names_from = age_class) %>%
+  select(-sim) %>%
+  distinct() -> dem_ests_all
+
+# sample 1000 of these par values
+set.seed(1222)
+dem_ests_sim <- dem_ests_all[sample(nrow(dem_ests_all), 1000), ]
+
+# Average # of pups born to a dog each year = 4.9 pups * 1 / 0.4
+pups_per_dogyr <- 5 * 1 * 0.4 # from Annie's thesis & Anna's
+
+# parallelize this (otherwise it takes a while...)
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+
+vacc_sims <- 
+  foreach(i = iter(dem_ests_sim, "row"), 
+          .export = c("simvacc", "bind_rows", "tibble"),
+          .combine = 'rbind') %dopar% {
+            
+            # get the prop
+            prop_pup <- as.numeric(i[, "1"])
+            prop_adult <- 1 - prop_pup
+            
+            # take the vals from optim and convert to adult survival
+            pup_surv <- (i$fert_est/pups_per_dogyr)^(1/12) # annual to monthly 
+            adult_surv <- i$psurv_adult_est^(1/12)
+            
+            # with campaigns only
+            camp <- simvacc(tmax = 12*10, 
+                            pup_surv = pup_surv, 
+                            adult_surv = adult_surv, 
+                            revacc = 1, 
+                            pup_cov = 0, 
+                            campaign_cov = 0.7, 
+                            campaign_times = seq(12, 12*10, by = 12), 
+                            pup_month_vacc = 1, 
+                            birthrate = 2.4/12, # monthly birth rate
+                            S_adult_0 = prop_adult*1000, 
+                            S_pup_0 = rep(prop_pup/12, 12)*1000, 
+                            V_adult_0 = 0, 
+                            V_pup_0 = rep(0, 12))
+            
+            # with pup vacc only
+            pup <- simvacc(tmax = 12*10, 
+                           pup_surv = pup_surv, 
+                           adult_surv = adult_surv, 
+                           revacc = 1, 
+                           pup_cov = 0.7, 
+                           campaign_cov = 0, campaign_times = 0, 
+                           pup_month_vacc = 3, 
+                           birthrate = 3/12, # monthly birth rate
+                           S_adult_0 = prop_adult*1000, 
+                           S_pup_0 = rep(prop_pup/12, 12)*1000, 
+                           V_adult_0 = 0, 
+                           V_pup_0 = rep(0, 12))
+            
+            both <- simvacc(tmax = 12*10, 
+                            pup_surv = pup_surv, 
+                            adult_surv = adult_surv, 
+                            revacc = 1, 
+                            pup_cov = 0.7, 
+                            campaign_cov = 0.7, 
+                            campaign_times = seq(12, 12*10, by = 12), 
+                            pup_month_vacc = 3, 
+                            birthrate = 3/12, # monthly birth rate
+                            S_adult_0 = prop_adult*1000, 
+                            S_pup_0 = rep(prop_pup/12, 12)*1000, V_adult_0 = 0, 
+                            V_pup_0 = rep(0, 12))
+            
+            vacc_comp <- bind_rows(tibble(month = 1:120, 
+                                          cov = camp$V/(camp$S + camp$V), 
+                                          type = "Campaign", 
+                                          pups_per_dogyr, 
+                                          pup_surv = pup_surv, 
+                                          adult_surv = adult_surv), 
+                                   tibble(month = 1:120, 
+                                          cov = pup$V/(pup$S + pup$V), 
+                                          type = "Puppy vax", 
+                                          pups_per_dogyr, 
+                                          pup_surv = pup_surv, 
+                                          adult_surv = adult_surv), 
+                                   tibble(month = 1:120,
+                                          cov = both$V/(both$S + both$V), 
+                                          type = "Both", 
+                                          pups_per_dogyr, 
+                                          pup_surv = pup_surv, 
+                                          adult_surv = adult_surv))
+            vacc_comp
+          }
+
+write_csv(vacc_sims, "out/vacc_sims.csv")
+
+# Close out
+stopCluster(cl)
